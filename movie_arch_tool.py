0f74fe9484485d593e4db870c3f0158a31bcea04
Sky1Wu <wtx919463944@gmail.com>
2019-10-09 20:49:05 +0800

    支持了 Windows 端自动修改文件夹图标

--- movie_arch_tool.py
+++ movie_arch_tool.py
@@ -1,8 +1,18 @@
+import win32api
 import os
 import requests
 import sys
 import shutil
 from bs4 import BeautifulSoup
+import platform
+
+if platform.system() == 'Windows':
+    import ctypes
+    from PIL import Image
+    import win32api
+    import win32con
+    from ctypes import POINTER, Structure, c_wchar, c_int, sizeof, byref
+    from ctypes.wintypes import BYTE, WORD, DWORD, LPWSTR, LPSTR
 
 os.chdir(sys.path[0])
 dirs = os.listdir('.')
@@ -40,7 +50,74 @@ for file_name in dirs:
             os.mkdir(path)
 
         shutil.move(file_name, path)
-        path += movie_title
-        with open(path+'.jpg', 'wb') as file:
+        img_path = path + movie_title+'.jpg'
+        with open(img_path, 'wb') as file:
             file.write(pic_add.content)
         file.close()
+
+        if platform.system() == 'Windows':
+            img = Image.open(img_path)
+            x, y = img.size
+            x = int(256/y*x)
+
+            img = img.resize((x, 256), Image.ANTIALIAS)
+
+            img_new = Image.new('RGBA', (256, 256), (0, 0, 0, 0))
+
+            img_new.paste(img, (int((256 - x) / 2), 0))
+
+            img_new.save(path+'favicon.ico', "ico", quality=100)
+
+            folderpath = path
+            iconpath = 'favicon.ico'
+
+            '''
+            此处修改文件夹图标的方法来自于 https://stackoverflow.com/questions/4662759/how-to-change-folder-icons-with-python-on-windows
+            '''
+            HICON = c_int
+            LPTSTR = LPWSTR
+            TCHAR = c_wchar
+            MAX_PATH = 260
+            FCSM_ICONFILE = 0x00000010
+            FCS_FORCEWRITE = 0x00000002
+            SHGFI_ICONLOCATION = 0x000001000
+
+            class GUID(Structure):
+                _fields_ = [
+                    ('Data1', DWORD),
+                    ('Data2', WORD),
+                    ('Data3', WORD),
+                    ('Data4', BYTE * 8)]
+
+            class SHFOLDERCUSTOMSETTINGS(Structure):
+                _fields_ = [
+                    ('dwSize', DWORD),
+                    ('dwMask', DWORD),
+                    ('pvid', POINTER(GUID)),
+                    ('pszWebViewTemplate', LPTSTR),
+                    ('cchWebViewTemplate', DWORD),
+                    ('pszWebViewTemplateVersion', LPTSTR),
+                    ('pszInfoTip', LPTSTR),
+                    ('cchInfoTip', DWORD),
+                    ('pclsid', POINTER(GUID)),
+                    ('dwFlags', DWORD),
+                    ('pszIconFile', LPTSTR),
+                    ('cchIconFile', DWORD),
+                    ('iIconIndex', c_int),
+                    ('pszLogo', LPTSTR),
+                    ('cchLogo', DWORD)]
+
+            shell32 = ctypes.windll.shell32
+
+            fcs = SHFOLDERCUSTOMSETTINGS()
+            fcs.dwSize = sizeof(fcs)
+            fcs.dwMask = FCSM_ICONFILE
+            fcs.pszIconFile = iconpath
+            fcs.cchIconFile = 0
+
+            shell32.SHGetSetFolderCustomSettings(
+                byref(fcs), folderpath, FCS_FORCEWRITE)
+
+            win32api.SetFileAttributes(
+                path+'favicon.ico', win32con.FILE_ATTRIBUTE_HIDDEN)
+            os.remove(img_path)
